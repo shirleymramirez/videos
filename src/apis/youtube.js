994af72fe16e3c52a4680087265ec777e88084a1
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-02-12 21:41:03 -0700

    added axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyAz6WlXLCcK_dAvv6DuFRO3CjkWM_3LdMU";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
