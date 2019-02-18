4cd6af4a6f9e387b6e08fa7c729e31af376ec47b
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-02-17 18:17:22 -0700

    Updates

---
+++ precache-manifest.c02d95bad3d5c334b280fcb9b8d4b5bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb30758431df4dcb2543",
+    "url": "/videos/static/js/runtime~main.fb307584.js"
+  },
+  {
+    "revision": "54768d8c20c1cbb1bc2b",
+    "url": "/videos/static/js/main.54768d8c.chunk.js"
+  },
+  {
+    "revision": "a61017d6ff38c783247f",
+    "url": "/videos/static/js/1.a61017d6.chunk.js"
+  },
+  {
+    "revision": "54768d8c20c1cbb1bc2b",
+    "url": "/videos/static/css/main.873324cb.chunk.css"
+  },
+  {
+    "revision": "ac0d60e2ab241d496451ded9e1cf9c67",
+    "url": "/videos/index.html"
+  }
+];
